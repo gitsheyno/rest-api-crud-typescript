7b483d10fe1b99d7104957d1dc77273ac570716b
shayan heidary <hdrydeveloper@gmail.com>
2023-10-24 17:05:08 +0200

    post,put operator

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -27,3 +27,51 @@ export async function DELETE(request: Request) {
 
   return NextResponse.json({ message: "todo deleted" });
 }
+
+export async function POST(request: Request) {
+  const { userId, title }: Partial<Todo> = await request.json();
+  //   const data: Todo = await request.json();
+
+  if (!userId || !title)
+    return NextResponse.json({ message: "data requeired" });
+
+  const res = await fetch(`${DATA_SOURCE_URL}`, {
+    method: "POST",
+    body: JSON.stringify({
+      userId,
+      title,
+      completed: false,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+  });
+  console.log(res);
+  const newTodo: Todo = await res.json();
+  return NextResponse.json(newTodo);
+}
+
+export async function PUT(request: Request) {
+  const { userId, title, completed, id }: Todo = await request.json();
+  //   const data: Todo = await request.json();
+
+  if (!userId || !title || !id || typeof completed !== "boolean")
+    return NextResponse.json({ message: "data requeired" });
+
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      userId,
+      title,
+      completed,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+  });
+  console.log(res);
+  const updatedTodo: Todo = await res.json();
+  return NextResponse.json(updatedTodo);
+}
